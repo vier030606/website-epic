05714d68892171bb6d7828f6ff07b965498a8a62
Ethan Cannavaro Lauda <javierky366@gmail.com>
2026-01-13 21:13:53 +0700

    Benerin error 419

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->validateCsrfTokens(except: [
+            'api/validate-code', 
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
